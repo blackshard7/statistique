19b2a10379ea2b226bd250c4a771f9d481e7b435
blackshard7 <84794552+blackshard7@users.noreply.github.com>
2024-10-01 09:10:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #from  paramPos import afficheParamPos
-#from somme import afficheSomme
+from somme import afficheSomme
 liste =[1.0,2.0,3.0]
 
 print("\n Data list :")
 print(list)
 
 print("\n ----------------")
-#print(afficheSomme(liste))
+print(afficheSomme(liste))
 print("\n ----------------")
 #print(afficheParamPost(liste))
---
+++ somme.py
@@ -0,0 +1,6 @@
+def afficheSomme(data):
+    somme =0.0
+   somme=sum(data)
+    nbre=len(data)
+
+    return "Somme =" +str(somme) + "\n" + "nombre d'éléments :" +str(nbre)
